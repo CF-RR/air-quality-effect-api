5b839a523eb8adefeefe95d3e035cf9761746b60
Rocky Randalls <rrandalls@gmail.com>
2015-03-06 13:14:33 -0500

    Add httparty and AirQuality#initialize

--- app/models/air_quality.rb
+++ app/models/air_quality.rb
@@ -1,2 +1,13 @@
 class AirQuality
+
+  def initialize(location)
+    @location = location.split.join("+")
+    @content = HTTParty.get(
+        "https://breezometer.p.mashape.com/location/?lang=en&location=#{@location}",
+        :headers => {
+        "X-Mashape-Key" => "5Jh6rr6h2Pmsh7bxyXY1dN1QhkJ8p19qMiwjsnQdf6HGXRuhtd",
+        "Accept" => "application/json"
+        }
+    )
+  end
 end
