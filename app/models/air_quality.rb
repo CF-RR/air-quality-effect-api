986c8dcfdbc548104e0c54d142e933bb2a5c6a14
Rocky Randalls <rrandalls@gmail.com>
2015-03-06 13:53:14 -0500

    Add http query method

--- app/models/air_quality.rb
+++ app/models/air_quality.rb
@@ -1,2 +1,13 @@
 class AirQuality
+
+  def initialize(location)
+    @location = location.split.join("+")
+    @content = HTTParty.get(
+        "https://breezometer.p.mashape.com/location/?lang=en&location=#{@location}",
+        :headers => {
+        "X-Mashape-Key" => "#{ENV['BREEZEOMETER_TOKEN']}",
+        "Accept" => "application/json"
+        }
+    )
+  end
 end
